d3a937e155c9c0dbaa7b635029da01cdecfd2007
Jordan Judt <dev@jordanjudt.com>
2018-03-28 14:31:16 -0500

    Added PHP projects

---
+++ PhpProject1/dblogin.php
@@ -0,0 +1,5 @@
+<?php
+    $db_user = "root";
+    $db_password = "doug401";
+    $db_name = "cratedb";
+    $db_host = "127.0.0.1";
--- PhpProject1/index.php
+++ PhpProject1/index.php
@@ -9,19 +9,26 @@ and open the template in the editor.
         <meta charset="UTF-8">
         <title>Cool Database Stuff</title>
         <?php
-        require "dbHelper.php";
-        
+        require "dblogin.php";
         ?>
     </head>
     <body>
         <?php
+        if (!$connection = mysqli_connect($db_host, $db_user, $db_password, $db_name))
+            die("Could not connect to database: " . mysqli_connect_error());
 
-        getPuzzles();
-        
-        
+        if (!$connection || !isset($connection))
+            die("Could not get puzzles: not connected");
+
+        if (!$query = mysqli_query($connection, "SELECT puzname FROM puzzles"))
+            die("Could not get puzzles: " . mysqli_error($connection));
+
+        while ($result = mysqli_fetch_assoc($query)) {
+            $out .= $result["puzname"] . "<br />";
+        }
         print $out;
 
-        disconnect();
+        mysqli_close($connection);
         ?>
     </body>
 </html>
