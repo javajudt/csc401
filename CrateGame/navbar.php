a4914fa30c6cbbe4267f636b434f7e7f90ca446e
Jordan.Judt <Jordan.Judt@LEIA09.usd.local>
2018-04-05 10:29:46 -0500

    Added some half-assed styling

--- CrateGame/navbar.php
+++ CrateGame/navbar.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This function facilitates creating the navbar across web pages. This way if 
  * something changes, the change is automatically applied to all pages.
@@ -9,14 +10,16 @@ function printNavbar($calling_page) {
     $pages['crategame'] = "Play";
     $pages['puzzlelist'] = "Puzzle List";
     $pages['signup'] = "Sign Up";
-    
+
     $nav = "";
     foreach ($pages as $key => $page) {
         $selected = strpos($calling_page, $key);
         $nav .= "<li class='";
-        if ($selected) $nav .= "selected";
+        if ($selected)
+            $nav .= "selected";
         $nav .= "'><a class='";
-        if ($selected) $nav .= "selected";
+        if ($selected)
+            $nav .= "selected";
         $nav .= "' href='$key.php'>$page</a></li>";
     }
     return $nav;
--- CrateGame/puzzlelist.php
+++ CrateGame/puzzlelist.php
@@ -20,15 +20,15 @@
         <table>
             <th class='big'>Puzzle Name</th>
             <th class='big'>Record Score</th>
-        <?php
-        if (!$connection = mysqli_connect($db_host, $db_user, $db_password, $db_name))
-            die("Could not connect to database: " . mysqli_connect_error());
+                <?php
+                if (!$connection = mysqli_connect($db_host, $db_user, $db_password, $db_name))
+                    die("Could not connect to database: " . mysqli_connect_error());
 
-        if (!$query = mysqli_query($connection, "SELECT * FROM $puzzle_table"))
-            die("Could not get puzzles: " . mysqli_error($connection));
-        
-        while ($result = mysqli_fetch_assoc($query)) {
-            print <<<ENDLI
+                if (!$query = mysqli_query($connection, "SELECT * FROM $puzzle_table"))
+                    die("Could not get puzzles: " . mysqli_error($connection));
+
+                while ($result = mysqli_fetch_assoc($query)) {
+                    print <<<ENDLI
 <tr class='clickable-row' onclick="window.location='crategame.php?id={$result["seedlev"]}';">
     <td>
         <p class='big'>{$result["puzname"]}</p>
@@ -40,10 +40,10 @@
     </td>
 </tr>
 ENDLI;
-        }
+                }
 
-        mysqli_close($connection);
-        ?>
+                mysqli_close($connection);
+                ?>
         </table>
     </body>
 </html>
